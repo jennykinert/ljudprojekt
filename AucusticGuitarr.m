09eeb1e4109d1f57ba77d94043e464e0efaab421
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2016-03-14 10:29:25 +0100

    Add aucustic guitarr

---
+++ AucusticGuitarr.m
@@ -0,0 +1,15 @@
+function [ tone ] = AucusticGuitarr( fs, chord )
+%AucusticGuitarr Creates a bass tone
+
+signal=randn(fs ,1);
+signal2=sin(exp(-100.*linspace(0,1,fs)));
+signTot = signal.*signal2';
+filter = zeros([1, length(signTot)]);
+N = 20;
+filter(1:N) = 1;
+filter = filter./N;
+signTot = conv(filter, signTot);
+tone = GenerateTone(signTot, fs, chord);
+
+end
+
