b7eb4a86bff68d19a422ec8a38ca056e39e78cf5
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2016-03-14 15:34:51 +0100

    Changes

--- AucusticGuitarr.m
+++ AucusticGuitarr.m
@@ -1,11 +1,12 @@
-function [ tone ] = AucusticGuitarr( duration, fs, chord )
+function [ tone ] = AucusticGuitarr( duration, fs, chord, amplitud )
 %AucusticGuitarr Creates a bass tone
 
 signal=randn(fs ,1);
-signal2=sin(exp(-100.*linspace(0,1,fs)));
+signal = signal.*amplitud;
+signal2=sin(exp(-100.*linspace(0,round(duration.*fs),fs)));
 signTot = signal.*signal2';
 filter = zeros([1, length(signTot)]);
-N = 20;
+N = 50;
 filter(1:N) = 1;
 filter = filter./N;
 signTot = conv(filter, signTot);
--- playSong.m
+++ playSong.m
@@ -1,17 +1,39 @@
 midi = readmidi('mountain_king.midi');
 [f0, duration, starttime, amplitude] = analyze(midi);
 fs = 44100;
-song = zeros([1, round((starttime(end) + duration(end))*fs)]);
+song = zeros([1, round((starttime(end) + duration(end)+25)*fs)]);
 
+tones = []
+freq = []
+lens = []
+maxtones = 0
+found = 0;ss
 for i = 1:length(f0)
     pos=round(starttime(i)*fs);
     pos = pos+1
-    dur = round(duration(i)*fs);
-    chord = AucusticGuitarr(round(duration(i)), fs, f0(i))';
-    size(chord)
-    size(song(1,pos:(pos+dur)))
-    song(1,pos:(pos+dur)) = (song(1,pos:(pos+dur)) + chord);
-end
 
+    nok = 0;
+    id = 1;
+    while (nok == 0) && (id <= maxtones)
+        if (lens(id) == duration(i)) && (freq(id) == f0(i))
+            nok = id;
+        end
+        id = id + 1;
+    end
+    size(tones);
+    if nok == 0
+        chord = AucusticGuitarr(duration(i), fs, f0(i), amplitude(i))';
+        tones = [tones; chord];
+        freq = [freq f0(i)];
+        lens = [lens duration(i)];
+        maxtones = maxtones + 1;
+    else
+        found = found + 1
+        chord = tones(nok,:);
+    end
+    
+    dur = length(chord)-1;
+    song(1,pos:(pos+dur)) = song(1,pos:(pos+dur)) + chord;
+end
 plot(song)
 sound(song, fs)
